07066cc8ac7112da4834cf977258da302a8f474b
jlnascimento <jlnascimento@pdmfc.com>
2019-10-14 18:14:17 +0100

    work in progress

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -5,16 +5,15 @@
       By <a
         href="#"
         class="link-unstyled"
-      >Robin</a>, <AppDate :timestamp="thread.publishedAt" />.
+      >Robin</a>,
+      <AppDate :timestamp="thread.publishedAt" />.
       <span
         style="float:right; margin-top: 2px;"
         class="hide-mobile text-faded text-small"
       >3 replies by 3 contributors</span>
     </p>
     <PostList :posts="posts" />
-    <PostEditor
-      :threadId="id"
-    />
+    <PostEditor :threadId="id" />
   </div>
 </template>
 
@@ -34,14 +33,10 @@ export default {
     }
   },
 
-  data () {
-    return {
-      thread: this.$store.state.threads[this.id],
-      newPostText: ''
-    }
-  },
-
   computed: {
+    thread () {
+      return this.$store.state.threads[this.id]
+    },
     posts () {
       const postIds = Object.values(this.thread.posts)
       return Object.values(this.$store.state.posts)
